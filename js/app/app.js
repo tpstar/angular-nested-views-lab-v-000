8e67133e1d6b95ecebaf397f47a09c26ee25dcf6
tpstar <hanlee12@gmail.com>
2017-01-14 23:19:11 -0600

    done

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,28 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'views/home.html'//,
+                // controller: 'UserController as user',
+                // resolve: {
+                //   userData: function ($stateParams, UserService) {
+                //       console.log($stateParams.id);
+                //       return UserService.getUser($stateParams.id);
+                //   }
+                // }
+            })
+            .state('home.notifications', {
+                url: 'notifications',
+                templateUrl: 'views/home/notifications.html'
+            })
+            .state('home.timeline', {
+              url: 'timeline',
+              templateUrl: 'views/home/timeline.html'
+            })
+            .state('home.user', {
+              url: 'user',
+              templateUrl: 'views/home/user.html'
+            })
+      });
